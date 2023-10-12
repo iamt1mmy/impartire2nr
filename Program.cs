594fe0e95572dcc7f8f33ae1d6005131ed90bdb8
iamt1mmy <146391141+iamt1mmy@users.noreply.github.com>
2023-10-12 22:25:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace impartirea2nr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int d, i, c;
+
+            d = Convert.ToInt32(Console.ReadLine());
+            i = Convert.ToInt32(Console.ReadLine());
+
+            c = d / i;
+
+            Console.WriteLine(c);
+        }
+    }
+}
